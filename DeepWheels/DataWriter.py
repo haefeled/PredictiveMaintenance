6bbbf1f622af5b696e671390d6a29be2c11bf8eb
fabianaust <aust.fabian@yahoo.de>
2020-06-28 22:45:36 +0200

    fixed DataWriter

--- DeepWheels/DataWriter.py
+++ DeepWheels/DataWriter.py
@@ -1,9 +1,13 @@
 import InfluxManager
+import DataPreparation
+import DataReader
+
 from influxdb import InfluxDBClient
+
 
 class DataWriter:
     def __init__(self, table_name):
-        self.influx_manager = InfluxManager()
+        self.influx_manager = InfluxManager.InfluxManager()
         self.table_name = table_name
         self.client = InfluxDBClient(host='localhost', port=8086)
         self.create_table()
@@ -13,5 +17,13 @@ class DataWriter:
         self.client.create_database(self.table_name)
         self.client.switch_database(self.table_name)
 
-if __name__ == '__main__':
+    def insert_data(self, data):
+        features = data[0].keys()
+        print(features)
 
+
+if __name__ == '__main__':
+    data = DataReader.load_data_from_sqlite3(r".\Data\AllData\example.sqlite3")
+    data = DataPreparation.sort_dict_into_list(data, True)
+    data_writer = DataWriter("test")
+    data_writer.insert_data(data)
