67629fd7b4e9df7500f912aa024fd756190a8881
Kaiser <daniel.kaiser@hs-furtwangen.de>
2020-06-28 22:46:22 +0200

    merge

--- DeepWheels/DeepWheels.py
+++ DeepWheels/DeepWheels.py
@@ -1,7 +1,15 @@
+from DataWriter import DataWriter
 
 
-def predict():
-    '''
+class DeepWheels:
+    def __init__(self):
+        self.predict_process = Process(target=predict, args=('bob',))
+        self.writer_process = Process(target=f, args=('bob',))
+        self.data_writer = DataWriter()
+    
+    def predict(self):
+        '''
+    
+        :return:
+        '''
 
-    :return:
-    '''
