c2ee98cd9da1d1517edf47b5f8879408e8b12bd1
fabianaust <aust.fabian@yahoo.de>
2020-06-29 00:29:07 +0200

    Example Database write & read

--- DeepWheels/DataWriter.py
+++ DeepWheels/DataWriter.py
@@ -1,4 +1,6 @@
-import InfluxManager
+from datetime import time
+
+from InfluxManager import InfluxManager
 import DataPreparation
 import DataReader
 
@@ -7,23 +9,40 @@ from influxdb import InfluxDBClient
 
 class DataWriter:
     def __init__(self, table_name):
-        self.influx_manager = InfluxManager.InfluxManager()
-        self.table_name = table_name
+        self.influx_manager = InfluxManager()
+        self.database_name = table_name
         self.client = InfluxDBClient(host='localhost', port=8086)
         self.create_table()
 
 
     def create_table(self):
-        self.client.create_database(self.table_name)
-        self.client.switch_database(self.table_name)
+        self.client.create_database(self.database_name)
+        self.client.switch_database(self.database_name)
 
     def insert_data(self, data):
-        features = data[0].keys()
-        print(features)
+        metrics = {}
+        metrics['measurement'] = "kartoffel"
+        metrics['tags'] = {}
+        metrics['fields'] = {}
+        for feature in data[0].keys():
+            metrics['fields'][feature] = data[0][feature]
+        self.client.write_points([metrics])
+
+    def print_data(self):
+
+        loginRecords = self.client.query('select * from kartoffel;')
+
+        # Print the time series query results
+
+        print(loginRecords)
+        bla = self.client.get_list_database()
+        print(bla)
 
 
 if __name__ == '__main__':
     data = DataReader.load_data_from_sqlite3(r".\Data\AllData\example.sqlite3")
     data = DataPreparation.sort_dict_into_list(data, True)
-    data_writer = DataWriter("test")
+    data_writer = DataWriter("testboom")
     data_writer.insert_data(data)
+    data_writer.print_data()
+
