55683e9a9f8861da1d17c4ca5e9a22e24319a142
fabianaust <aust.fabian@yahoo.de>
2020-07-14 17:42:37 +0200

    adapted DeepWheels

--- DeepWheels/DeepWheels.py
+++ DeepWheels/DeepWheels.py
@@ -15,7 +15,7 @@ class DeepWheels:
     def __init__(self):
         self.data_reader = DataReader()
         self.data_prep = DataPreparation()
-        self.data_writer = DataWriter("live_data2")
+        self.data_writer = DataWriter("live_data")
         self.last_session_writer = DataWriter("last_session_data")
         self.prep_writer = DataWriter("prep_data")
         self.data_predict = Predict()
@@ -33,7 +33,7 @@ class DeepWheels:
             data = self.data_reader.listen_udp(1)
             data = self.data_prep.sort_dict_into_list(data, False)
             self.data_writer.insert_data(data[0])
-            tmp_list.append(deepcopy(data))
+            tmp_list.append(deepcopy(data[0]))
         tmp_df = self.data_prep.list_to_dataframe(tmp_list)
         self.prep_list = deepcopy(tmp_df)
         predict_process = threading.Thread(target=Predict.predict, args=(self.data_predict, self.prep_list, self.prep_writer))
